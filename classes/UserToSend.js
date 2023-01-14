7a09b74aa1794919cbc548512ba63196eddf0267
jakubwiechniak <wiechniak.j@gmail.com>
2023-01-14 20:37:13 +0100

    merge

---
+++ classes/UserToSend.js
@@ -0,0 +1,14 @@
+class UserToSend {
+    constructor(id, email, name, surname, avatar, role, money, nick) {
+        this.id = id
+        this.nick = nick;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.money = money;
+        this.avatar = avatar;
+        this.role = role === "on" ? "Trainer" : "User"
+    }
+}
+
+module.exports = UserToSend
